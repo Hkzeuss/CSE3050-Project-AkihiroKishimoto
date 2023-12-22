1b376af9fcef008a339000a768d1a1b7dd1d7216
manh0107 <phamquangmanh0107@gmail.com>
2023-12-22 23:58:54 +0700

    upload A* and IDA* with h minout

---
+++ Astar_with_h_minout.py
@@ -0,0 +1,110 @@
+import math
+import random
+import heapq
+import time
+
+def generate_cost_matrix(num_cities, seed):
+    random.seed(seed)
+    cost_matrix = [[0] * num_cities for _ in range(num_cities)]
+
+    for i in range(num_cities):
+        for j in range(num_cities):
+            if i != j:
+                cost = random.randint(1, 100)
+                cost_matrix[i][j] = cost
+
+    return cost_matrix
+
+def a_star_min_out(cost_matrix, start, goal):
+    num_rows, num_cols = len(cost_matrix), len(cost_matrix[0])
+
+    def heuristic_min_out(node, visited_cities):
+        remaining_cities = [j for j in range(len(cost_matrix)) if j not in visited_cities]
+        if not remaining_cities:
+            return 0
+        return min(cost_matrix[node[0]][j] for j in remaining_cities)
+
+    # Priority queue to store nodes with their total cost
+    priority_queue = [(0, start)]
+
+    # Dictionary to store the best-known cost from start to each node
+    best_known_cost = {start: 0}
+
+    # Dictionary to store the parent node for each node
+    parents = {start: None}
+
+    start_time = time.time()
+
+    while priority_queue:
+        current_cost, current_node = heapq.heappop(priority_queue)
+
+        # Check if we reached the goal
+        if current_node == goal:
+            end_time = time.time()
+            solve_time = end_time - start_time
+
+            # Reconstruct the path from the goal to the start
+            path = []
+            current = goal
+            while current is not None:
+                path.insert(0, current)
+                current = parents[current]
+
+            return solve_time, best_known_cost[goal], path
+
+        # Explore neighbors
+        neighbors = [
+            (current_node[0] + 1, current_node[1]),
+            (current_node[0] - 1, current_node[1]),
+            (current_node[0], current_node[1] + 1),
+            (current_node[0], current_node[1] - 1)
+        ]
+
+        for neighbor in neighbors:
+            row, col = neighbor
+            if 0 <= row < num_rows and 0 <= col < num_cols:
+                total_cost = best_known_cost[current_node] + cost_matrix[row][col]
+
+                # Update best-known cost if a shorter path is found
+                if neighbor not in best_known_cost or total_cost < best_known_cost[neighbor]:
+                    best_known_cost[neighbor] = total_cost
+                    priority = total_cost + heuristic_min_out(neighbor, [current_node[0]])
+                    heapq.heappush(priority_queue, (priority, neighbor))
+                    parents[neighbor] = current_node
+
+    return float('inf'), float('inf'), []  # Return infinity if no path is found
+
+# Example usage:
+seeds = [1, 2, 3, 4, 5]
+num_cities_values = [5, 10, 11, 12]
+
+total_solve_time = 0
+
+for num_cities in num_cities_values:
+    for seed in seeds:
+        cost_matrix = generate_cost_matrix(num_cities, seed)
+
+        print(f"\nCost Matrix (Seed {seed}, Num Cities {num_cities}):")
+        for row in cost_matrix:
+            print(row)
+
+        start_node = (0, 0)
+        goal_node = (num_cities - 1, num_cities - 1)
+
+        start_time = time.time()
+        solve_time, total_cost, path = a_star_min_out(cost_matrix, start_node, goal_node)
+        end_time = time.time()
+
+        total_solve_time += solve_time
+
+        print("\nOptimal Path:")
+        if path:
+            for node in path:
+                print(f"({node[0]}, {node[1]}) - Cost: {cost_matrix[node[0]][node[1]]}")
+        else:
+            print("No path found.")
+
+        print(f"\nTotal Cost: {'Infinity' if math.isinf(total_cost) else total_cost}")
+        print(f"Solve Time: {solve_time} seconds")
+
+print(f"\nAverage Solve Time: {total_solve_time / (len(seeds) * len(num_cities_values))} seconds\n")
---
+++ IDAstar_with_h_minout.py
@@ -0,0 +1,81 @@
+import sys
+import random
+import time
+
+def generate_cost_matrix(num_cities, seed):
+    random.seed(seed)
+    cost_matrix = [[0] * num_cities for _ in range(num_cities)]
+
+    for i in range(num_cities):
+        for j in range(num_cities):
+            if i != j:
+                cost = random.randint(1, 100)
+                cost_matrix[i][j] = cost
+
+    return cost_matrix
+
+def ida_star_min_out(cost_matrix, start, goal):
+    def heuristic(node, visited_cities):
+        # Calculate the min-out heuristic
+        remaining_cities = [j for j in range(len(cost_matrix)) if j not in visited_cities]
+        if not remaining_cities:
+            return 0
+        return min(cost_matrix[node][j] for j in remaining_cities)
+
+    def dfs(current, g, bound, path, visited_cities):
+        f = g + heuristic(current, visited_cities)
+        if f > bound:
+            return f, path
+        if current == goal:
+            return "FOUND", path + [current]
+
+        min_cost = float('inf')
+        next_path = None
+
+        for neighbor in range(len(cost_matrix[current])):
+            if cost_matrix[current][neighbor] > 0 and neighbor not in visited_cities:
+                new_visited_cities = visited_cities + [neighbor]
+                result, new_path = dfs(neighbor, g + cost_matrix[current][neighbor], bound, path + [neighbor], new_visited_cities)
+                if result == "FOUND":
+                    return "FOUND", new_path
+                if result < min_cost:
+                    min_cost = result
+                    next_path = new_path
+
+        return min_cost, next_path
+
+    bound = heuristic(start, [])
+    path = [start]
+    start_time = time.time()
+    while True:
+        result, path = dfs(start, 0, bound, path, [])
+        end_time = time.time()
+        if result == "FOUND":
+            return path, bound, end_time - start_time
+        if result == float('inf'):
+            return "No path found", None, end_time - start_time
+        bound = result
+
+# Example usage:
+seeds = [1, 2, 3, 4, 5]
+num_cities_values = [5, 10, 11, 12]
+
+for num_cities in num_cities_values:
+    for seed in seeds:
+        cost_matrix = generate_cost_matrix(num_cities, seed)
+
+        print(f"\nCost Matrix (Seed {seed}, Num Cities {num_cities}):")
+        for row in cost_matrix:
+            print(row)
+
+        start_node = 0  # Assuming the starting node is 0
+        goal_node = num_cities - 1  # Assuming the goal node is the last node
+
+        print("\nRunning IDA* with min-out heuristic...")
+        path, cost, run_time = ida_star_min_out(cost_matrix, start_node, goal_node)
+
+        print("\nOptimal Path:")
+        for node in path:
+            print(f"({node // num_cities}, {node % num_cities}) - Cost: {cost_matrix[node // num_cities][node % num_cities]}")
+        print(f"Shortest Path Cost: {cost}")
+        print(f"Run Time: {run_time} seconds\n")
